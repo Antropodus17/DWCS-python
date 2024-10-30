2f634eee565e5b1b9af9be2f448c627882b7cf1e
a23sergiopn <a23sergiopn@iessanclemente.net>
2024-10-30 11:35:42 +0100

    Ejercicio de clases

--- Clases.py
+++ Clases.py
@@ -1,61 +1,77 @@
-"""docstring for Car."""
+"""Persona"""
 
 
-class Car:
+class Person:
     """Constructor"""
 
-    def __init__(self, color, power):
-        self.color = color
-        self.power = power
+    def __init__(self, id, name, age):
+        self.setId(id)
+        self.setName(name)
+        self.setAge(age)
 
-    def __str__(self):
-        return f"This car is :self.color and have :self.power horse"
+    """SETTERS"""
+
+    def setId(self, id):
+        self.id = str(id)
+
+    def setName(self, name):
+        self.name = str(name)
+
+    def setAge(self, age):
+        self.age = int(age)
+
+    """TO STRING"""
+
+    def __str__(self, tab=0):
+        return f"{"\t"*tab}Id: {self.id}\n{"\t"*tab}Name: {self.name}\n{"\t"*tab}Age: {self.age}"
 
 
-"""Class Calculator for make calculations"""
+"""Estudiante"""
 
 
-class Calculator:
+class Student:
 
-    numberOfObjects = 0
+    def __init__(self, id, degree, person):
+        self.setId(id)
+        self.setDegree(degree)
+        self.setPerson(person)
 
-    def __init__(self, num1=0, num2=0):
-        self.setNum1(num1)
-        self.setNum2(num2)
-        Calculator.numberOfObjects += 1
+    def setId(self, id):
+        self.id = str(id)
 
-    """Setter for the num1"""
+    def setDegree(self, degree):
+        self.degree = str(degree)
 
-    def setNum1(self, num):
-        if type(num) != int:
-            raise TypeError("The number must be a integer")
+    def setPerson(self, person):
+        if type(person) == Person:
+            self.person = person
+        else:
+            raise TypeError("Person must be of type Person")
 
-        self.num1 = num
+    def __str__(self, tab=0):
+        return f"{"\t"*tab}Id: {self.id}\n{"\t"*tab}Degree: {self.degree}\n{"\t"*tab}Person: {self.person}"
+    
 
-    """Getter of the num2"""
+"""Grupo de estudiantes"""
+class studentGroup:
 
-    def getNumb1(self):
-        return self.num1
+    """Constructor"""
+    def __init__(self, id,name ,students):
+        self.setId(id)
 
-    """Setter for the num2"""
+    """SETTERS"""
+    def setId(self,id):
+        self.id=str(id)
 
-    def setNum2(self, num):
-        if type(num) != int:
-            raise TypeError("The number must be a integer")
+    def setName(self,name):
+        self.name=str(name)
 
-        self.num2 = num
-
-    """Getter of the num2"""
-
-    def getNumb2(self):
-        return self.num2
-
-    """To string of the Calculator"""
-
-    def __str__(self):
-        return f"Numero 1: {self.num1}\nNumero 2: {self.num2}\nSuma: {self.sum()}"
-
-    """Method to return the sum of the two nums"""
-
-    def sum(self):
-        return self.num1 + self.num2
+    def setStudents(self,students):
+        self.students=[]
+        for student in students:
+            if type(student)==Student:
+                self.students.append(student)
+    """TO STRING"""
+    def __str__(self) -> str:
+        pass
+    
