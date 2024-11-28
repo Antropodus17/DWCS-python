bb07bb7c737c53171f209c9f4e2287f9ab82a7dd
Antropodus17 <a23sergiopn@iessanclemente.net>
2024-11-28 20:22:50 +0100

    ejercicio Exceptions hecho

--- Clases.py
+++ Clases.py
@@ -1,61 +1,7 @@
-"""docstring for Car."""
+class DivisionByZeroError(Exception):
+
+    def __init__(self, message: str):
+        super(Exception, self).__init__(message)
 
 
-class Car:
-    """Constructor"""
-
-    def __init__(self, color, power):
-        self.color = color
-        self.power = power
-
-    def __str__(self):
-        return f"This car is :self.color and have :self.power horse"
-
-
-"""Class Calculator for make calculations"""
-
-
-class Calculator:
-
-    numberOfObjects = 0
-
-    def __init__(self, num1=0, num2=0):
-        self.setNum1(num1)
-        self.setNum2(num2)
-        Calculator.numberOfObjects += 1
-
-    """Setter for the num1"""
-
-    def setNum1(self, num):
-        if type(num) != int:
-            raise TypeError("The number must be a integer")
-
-        self.num1 = num
-
-    """Getter of the num2"""
-
-    def getNumb1(self):
-        return self.num1
-
-    """Setter for the num2"""
-
-    def setNum2(self, num):
-        if type(num) != int:
-            raise TypeError("The number must be a integer")
-
-        self.num2 = num
-
-    """Getter of the num2"""
-
-    def getNumb2(self):
-        return self.num2
-
-    """To string of the Calculator"""
-
-    def __str__(self):
-        return f"Numero 1: {self.num1}\nNumero 2: {self.num2}\nSuma: {self.sum()}"
-
-    """Method to return the sum of the two nums"""
-
-    def sum(self):
-        return self.num1 + self.num2
+Exception
---
+++ ejercicio3.17.py
@@ -0,0 +1,28 @@
+from Clases import DivisionByZeroError
+from colorama import Fore
+
+continuar = True
+
+# FUNCTIONS
+
+
+def divide_numbers(numerator, denominator):
+    if denominator == 0:
+        raise DivisionByZeroError("Cannot divide by zero.")
+    else:
+        return numerator / denominator
+
+
+while continuar:
+    numerator = int(input(Fore.GREEN + "Introduce the Numerator: "))
+    denominator = int(input("Introduce the Denominator: "))
+    try:
+        print(divide_numbers(numerator, denominator))
+    except DivisionByZeroError as e:
+        print(Fore.RED + e.__str__() + Fore.GREEN)
+    finally:
+        if (
+            input(Fore.LIGHTBLUE_EX + "You want to make another divition?(y|N): ")
+            != "y"
+        ):
+            continuar = False
